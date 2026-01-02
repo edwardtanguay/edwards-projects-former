ab3eaeea653616968b7e2fca7457b7a485e6253d
Edward Tanguay (windows) <edward@tanguay.info>
2026-01-02 12:00:04 +0100

    FEATURE: Add prompt example

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -64,11 +64,15 @@ const startProjects = _projects.filter((project) =>
       <h2 class="text-2xl font-bold mb-3">Starter Sites</h2>
       <p>These are starter sites for new projects created with various AI tools.</p>
       <p>Click on the tabs below to see screenshots and links to live sites and repos, so you can decide which you want to use as starters for your projects.</p>
-      <p class="mb-6">
+      <p class="mb-3">
         NOTE: These starter sites will eventually be incorporated into my new site: (<a class="underline" target="_blank" href="https://edwards-projects.vercel.app"
           >edwards-projects.vercel.app</a
         >), after which this site will be deleted.
       </p>
+      <p class="mb-3">Here is a prompt example when creating these sites:</p>
+      
+     <p class="italic mb-6 font-mono text-orange-900">
+       create a Astro site with one page that displays employee records from a JSON file, it enables the user to create, edit, delete, sort and search employees, the employees are stored in a JSON file and CRUD calls are made to backend Node/Express server, the employee records have 7 fields of type string, number and date, the site has TypeScript, Tailwind 4, and a professional but reduced look with light gray, the site is responsive and cleverly solves horizonal scrolling of all fields in mobile view so that it is easy to edit records on a smartphone, use icons where appropriate, the Node/Express server and Angular can be started simultaneously with one "npm run dev" command</p>
       <DisplayProject
         v-for="project in startProjects"
         :key="project.suuid"
